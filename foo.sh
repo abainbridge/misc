5e6f24c5eff3ddd0607b04d943a34b3266f8051f
Andrew Bainbridge <andbain@microsoft.com>
2018-03-26 09:57:47 +0100

    sdf

--- foo.sh
+++ foo.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Add stuff to .bashrc
-# cat <<'heredoc1' >>/home/epcadmin/.bashrc
-#     #    < Set window title > <- Cyan   ->        <- Yellow ->   <- Green -->  <Normal >
-#     PS1='\[\033]0;\u@\h\007\] \[\e[1;36m\]\n\u@\h \[\e[1;33m\]\t \[\e[1;32m\]\w\[\e[0m\]\n\$ '
-# 
-#     # Update the BASH history file after each command, so if the sessions crashes/is
-#     # terminated, we get to keep the history
-#     shopt -s histappend
-#     PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
-# 
-#     PROMPT_COMMAND="pwd > ~/.lastdir;$PROMPT_COMMAND"
-#     cd `cat ~/.lastdir`
-# 
-#     shopt -s checkwinsize
-#     shopt -s no_empty_cmd_completion
-#     shopt -s nocaseglob         # Make the * wildcard case insensitive
-#     export HISTCONTROL=ignoreboth:erasedups
-# 
-#     alias ls='ls -oAF --color --group-directories-first'
-# heredoc1
+cat <<'heredoc1' >>/home/epcadmin/.bashrc
+    #    < Set window title > <- Cyan   ->        <- Yellow ->   <- Green -->  <Normal >
+    PS1='\[\033]0;\u@\h\007\] \[\e[1;36m\]\n\u@\h \[\e[1;33m\]\t \[\e[1;32m\]\w\[\e[0m\]\n\$ '
+
+    # Update the BASH history file after each command, so if the sessions crashes/is
+    # terminated, we get to keep the history
+    shopt -s histappend
+    PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
+
+    PROMPT_COMMAND="pwd > ~/.lastdir;$PROMPT_COMMAND"
+    cd `cat ~/.lastdir`
+
+    shopt -s checkwinsize
+    shopt -s no_empty_cmd_completion
+    shopt -s nocaseglob         # Make the * wildcard case insensitive
+    export HISTCONTROL=ignoreboth:erasedups
+
+    alias ls='ls -oAF --color --group-directories-first'
+heredoc1
 
 
 # Create udp_responder.py
